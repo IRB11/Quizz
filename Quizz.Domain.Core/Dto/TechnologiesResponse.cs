252455e1887493e50fdef0f593b33d22d897c071
IB <ibrahimrabai11@gmail.Com>
2024-05-09 20:13:30 +0200

    Create Technologies DTO

---
+++ Quizz.Domain.Core/Dto/TechnologiesResponse.cs
@@ -0,0 +1,15 @@
+﻿using Quizz.Domain.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.Dto
+{
+    public class TechnologiesResponse
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
