876a215687f83a984a3698a5525963d60c975440
IB <ibrahimrabai11@gmail.Com>
2024-05-09 20:12:52 +0200

    Create Quizz Answer DTO

---
+++ Quizz.Domain.Core/Dto/QuizAnswerRequest.cs
@@ -0,0 +1,16 @@
+﻿using Quizz.Domain.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.Dto
+{
+    public class QuizzAnswerRequest
+    {
+        public long? Id { get; set; }
+        public string Content { get; set; }
+        public AnswerOption AnswerOption { get; set; }
+    }
+}
