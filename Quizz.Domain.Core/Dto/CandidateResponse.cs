8bcfb181e842203df2bb49e4b351731696aba452
IB <ibrahimrabai11@gmail.Com>
2024-05-09 20:11:57 +0200

    Create Candidate DTO

---
+++ Quizz.Domain.Core/Dto/CandidateResponse.cs
@@ -0,0 +1,20 @@
+﻿using Quizz.Domain.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.Dto
+{
+    public class CandidateResponse
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAdress { get; set; }
+        public Agent Agent { get; set; }
+        public QuizzTest QuizzTest { get; set; }
+    }
+}
