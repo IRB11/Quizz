386b34480b5f96eea0b672cb6ba66c909db68313
IB <ibrahimrabai11@gmail.Com>
2024-05-09 20:12:09 +0200

    Create Level DTO

---
+++ Quizz.Domain.Core/Dto/LevelRequest.cs
@@ -0,0 +1,16 @@
+﻿using Quizz.Domain.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.Dto
+{
+    public class LevelRequest
+    {
+        public long? Id { get; set; }
+        public string Content { get; set; }
+    }
+}
