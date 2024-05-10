8e5e8a337b467a642f1b98421a64cf6df36e84c8
IB <ibrahimrabai11@gmail.Com>
2024-05-10 22:42:43 +0200

    Add level property to Admin

--- Quizz.Domain.Core/Entities/Admin.cs
+++ Quizz.Domain.Core/Entities/Admin.cs
@@ -18,5 +18,6 @@ namespace Quizz.Domain.Core.Entities
         public Question Question { get; set; }
         public QuizzTest QuizzTest { get; set; }
         public Technologies Technologies { get; set; }
+        public Level Level { get; set; }
     }
 }
