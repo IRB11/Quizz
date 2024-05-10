7320c0b002057bda373ec6f0c59d69f097fd26c9
IB <ibrahimrabai11@gmail.Com>
2024-05-10 21:36:16 +0200

    Register levelo repository as ilevelrepository

---
+++ Quizz.Domain.Infrastructure/QuizzDomainInfrastructureModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Quizz.Domain.Core.Interfaces;
+using Quizz.Domain.Core.Interfaces.Date;
+using Quizz.Domain.Infrastructure.Date;
+using Quizz.Domain.Infrastructure.Data.Repositories;
+
+namespace Quizz.Domain.Infrastructure
+{
+    public class QuizzDomainInfrastructureModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<LevelRepository>().As<ILevelRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<DateTimeService>().As<IDateTimeService>();
+            builder.RegisterType<CalendarService>().As<ICalendarService>();
+        }
+    }
+}
