45dcd2a5dd91245f7f6042b1ccbb31693d9d23a8
IB11 <ibrahimrabai11@gmail.Com>
2024-06-17 10:20:16 +0200

    Add Create user use case

---
+++ Quizz.Domain.Core/UseCases/CreateUser.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Microsoft.IdentityModel.Tokens;
+using Quizz.Domain.Core.Dto;
+using Quizz.Domain.Core.Interfaces;
+using Quizz.Domain.Core.Services;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.UseCases
+{
+    public class CreateUser : ICreateUser
+    {
+        private readonly IUserRepository userRepository;
+        private readonly IMapper mapper;
+        private readonly JWTService jwtService;
+
+        public CreateUser(IUserRepository userRepository, IMapper mapper, JWTService jWTService)
+        {
+            this.userRepository = userRepository;
+            this.mapper = mapper;
+            this.jwtService = jWTService;   
+        }
+
+        public async Task<UserResponse> Handle(UserRequest createUserRequest)
+        {
+            var response = await userRepository.CreateUser(createUserRequest);
+
+            if (response == null)
+            {
+                return null;
+            }
+
+            response.Token = jwtService.GetToken(response);
+
+            return response;
+        }
+    }
+}
