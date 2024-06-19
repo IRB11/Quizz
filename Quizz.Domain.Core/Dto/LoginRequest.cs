c847e6c8bb5de26cd6606db6912c8cde08c2513c
IB11 <ibrahimrabai11@gmail.Com>
2024-06-19 11:18:34 +0200

    Add Login request Dto and adjust User response

---
+++ Quizz.Domain.Core/Dto/LoginRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.Dto
+{
+    public class LoginRequest
+    {
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+
+    }
+}
