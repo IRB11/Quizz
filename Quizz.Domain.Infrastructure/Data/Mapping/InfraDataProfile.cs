3c998f1d78429d882ca733fe65b41180eb91bdcf
IB11 <ibrahimrabai11@gmail.Com>
2024-06-19 11:12:57 +0200

    Add mappin to User and UserRequest

--- Quizz.Domain.Infrastructure/Data/Mapping/InfraDataProfile.cs
+++ Quizz.Domain.Infrastructure/Data/Mapping/InfraDataProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Quizz.Domain.Core.Dto;
+using Quizz.Domain.Core.Entities;
 using Quizz.Domain.Infrastructure.Data.Entities;
 
 
@@ -13,6 +14,23 @@ namespace Quizz.Domain.Infrastructure.Data.Mapping
             ShouldMapProperty = propertyInfo => true;
 
             CreateMap<EFLevel, LevelRequest>().ReverseMap();
+
+            CreateMap<LoginRequest, EFUser>()
+                .ForMember(dest => dest.EmailAddress, opt => opt.MapFrom(src => src.EmailAddress))
+                .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password));
+
+            CreateMap<EFUser, UserResponse>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Role.Id));
+
+            CreateMap<UserRequest, EFUser>()
+                .ForMember(dest => dest.Role, opt => opt.Ignore());
+
+            CreateMap<EFUser, UserResponse>()
+                .ForMember(dest => dest.Role, opt => opt.Condition(src => src.Role != null))
+                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role));
+
+            CreateMap<EFRole, Role>();
+
             CreateMap<EFLevel, LevelResponse>().ReverseMap();
 
 
