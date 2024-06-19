be5763ec08883b65d1e616bb734f3d34c20987b9
IB11 <ibrahimrabai11@gmail.Com>
2024-06-19 11:11:42 +0200

    add user controller

---
+++ Quizz/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quizz.Domain.Core.Dto;
+using Quizz.Domain.Core.Interfaces;
+using Quizz.Domain.Core.UseCases;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Quizz.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ICreateUser createUser;
+
+        public UserController(ICreateUser user)
+        {
+            this.createUser = user;
+        }
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] UserRequest userRequest)
+        {
+            return Ok(await createUser.Handle(userRequest));
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
