de651bfbda6f6169df04fc312707bd0510482e8d
IB11 <ibrahimrabai11@gmail.Com>
2024-06-19 11:17:30 +0200

    Creation of interfaces

---
+++ Quizz.Domain.Core/Interfaces/ICreateUser.cs
@@ -0,0 +1,9 @@
+﻿using Quizz.Common.Interfaces;
+using Quizz.Domain.Core.Dto;
+
+namespace Quizz.Domain.Core.Interfaces
+{
+    public interface ICreateUser : IUseCaseRequestHandler<UserRequest, UserResponse>
+    {
+    }
+}
