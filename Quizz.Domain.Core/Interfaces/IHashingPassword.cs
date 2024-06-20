ffec1975ed12ed797ef8049314df4e2c3705d729
IB11 <ibrahimrabai11@gmail.Com>
2024-06-20 22:06:56 +0200

    Create HashPassword interface and service

---
+++ Quizz.Domain.Core/Interfaces/IHashingPassword.cs
@@ -0,0 +1,15 @@
+﻿using Quizz.Domain.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.Interfaces
+{
+    public interface IHashingPassword
+    {
+        public Task<string> CreateUser(User create);
+        public Task<string> UserVerify(User verify);
+    }
+}
---
+++ Quizz.Domain.Core/Services/HashingPassword.cs
@@ -0,0 +1,23 @@
+﻿using Quizz.Domain.Core.Entities;
+using Quizz.Domain.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.Services
+{
+    public class HashingPassword : IHashingPassword
+    {
+        public Task<string> CreateUser(User create)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UserVerify(User verify)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
