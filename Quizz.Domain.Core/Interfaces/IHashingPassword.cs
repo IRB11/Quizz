66e5a17f08e8787d5e3fc899961db325a0121a49
IB11 <ibrahimrabai11@gmail.Com>
2024-06-22 22:12:44 +0200

    Create an Interface and implement to handle password hashing and verify hashed password

--- Quizz.Domain.Core/Interfaces/IHashingPassword.cs
+++ Quizz.Domain.Core/Interfaces/IHashingPassword.cs
@@ -1,4 +1,5 @@
-﻿using Quizz.Domain.Core.Entities;
+﻿using Quizz.Domain.Core.Dto;
+using Quizz.Domain.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,8 @@ namespace Quizz.Domain.Core.Interfaces
 {
     public interface IHashingPassword
     {
-        public Task<string> CreateUser(User create);
-        public Task<string> UserVerify(User verify);
+        public Task<bool> UserVerify(LoginRequest loginRequest);
+        public string HashPassword(string password, byte[] salt);
+        public byte[] GenerateSalt();
     }
 }
