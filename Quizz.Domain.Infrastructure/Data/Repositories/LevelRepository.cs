ff86b7b71edab3cdffbfc25f309831942ae5e7dc
IB11 <ibrahimrabai11@gmail.Com>
2024-06-23 22:46:34 +0200

    Update InMemoryLevel Repo and level Repository for Read Update and Delete

--- Quizz.Domain.Infrastructure/Data/Repositories/LevelRepository.cs
+++ Quizz.Domain.Infrastructure/Data/Repositories/LevelRepository.cs
@@ -1,14 +1,7 @@
 ﻿using AutoMapper;
-using Microsoft.EntityFrameworkCore;
 using Quizz.Domain.Core.Dto;
 using Quizz.Domain.Core.Interfaces;
 using Quizz.Domain.Infrastructure.Data.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Quizz.Domain.Infrastructure.Data.Repositories
 {
@@ -17,7 +10,7 @@ namespace Quizz.Domain.Infrastructure.Data.Repositories
         private readonly Context context;
         private readonly IMapper mapper;
 
-        public LevelRepository(Context context, IMapper mapper) : base ()
+        public LevelRepository(Context context, IMapper mapper) : base()
         {
             this.context = context;
             this.mapper = mapper;
@@ -36,9 +29,67 @@ namespace Quizz.Domain.Infrastructure.Data.Repositories
             return Task.Run(() => context.Levels.Any(f => f.Content.ToLower().Equals(Content.ToLower())));
         }
 
+        public async Task<bool> DeleteLevel(LevelRequest levelRequest)
+        {
+            if (context.Levels.Any(l => l.Id == levelRequest.Id))
+            {
+                EFLevel eFLevel = mapper.Map<EFLevel>(levelRequest);
+                context.Levels.Remove(eFLevel);
+                context.SaveChanges();
+                return true;
+            }
+            else return false;
+        }
+
+        public async Task<List<LevelResponse>> getAllLevels()
+        {
+            var efLevels = context.Levels.ToList();
+            var levels = mapper.Map<List<LevelResponse>>(efLevels);
+
+            return levels;
+        }
+
+        public async Task<LevelResponse> GetLevel(int id)
+        {
+            EFLevel eFLevel = context.Levels.FirstOrDefault(l => l.Id == id);
+            LevelResponse levelResponse = mapper.Map<LevelResponse>(eFLevel);
+            return levelResponse;
+        }
+
         public Task<bool> IdIsNotAvailable(long? id)
         {
-            throw new NotImplementedException();
+            return Task.Run(() => context.Levels.Any(f => f.Id == id));
+        }
+
+        public Task<bool> levelIsUsed(LevelRequest levelRequest)
+        {
+            return Task.Run(() => context.Questions.Any(f => f.Level.Id == levelRequest.Id));
+        }
+
+        public async Task<LevelResponse> Update(LevelRequest levelRequest)
+        {
+            EFLevel efLevel = mapper.Map<EFLevel>(levelRequest);
+            LevelResponse levelResponse = null;
+            try
+            {
+                await Task.Run(() =>
+                {
+                    lock (context.Levels)
+                    {
+                        context.Levels.Update(efLevel);
+                        context.SaveChangesAsync();
+
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                levelResponse.Id = -1;
+                levelResponse.Content = $"An error occurred: {ex.Message}";
+            }
+            levelResponse = mapper.Map<LevelResponse>(efLevel);
+
+            return levelResponse;
         }
     }
 }
