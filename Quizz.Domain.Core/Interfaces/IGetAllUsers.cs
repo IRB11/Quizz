5f3d2b58490db835c37a0cd70386eb6b4017b072
IB11 <ibrahimrabai11@gmail.Com>
2024-07-02 21:10:56 +0200

    Feat / Backend : -User Add UseCase Crud interfaces

---
+++ Quizz.Domain.Core/Interfaces/IGetAllUsers.cs
@@ -0,0 +1,9 @@
+﻿using Quizz.Common.Interfaces;
+using Quizz.Domain.Core.Dto;
+
+namespace Quizz.Domain.Core.Interfaces
+{
+    public interface IGetAllUsers : IUseCaseRequestHandler<List<UserResponse>>
+    {
+    }
+}
