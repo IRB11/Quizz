11593d3cd033ee439803d71b94776a8e9783842e
IB11 <ibrahimrabai11@gmail.Com>
2024-07-02 21:21:49 +0200

    Feat / Backend :  fix dto issue

--- Quizz.Domain.Core/Dto/TechnologiesRequest.cs
+++ Quizz.Domain.Core/Dto/TechnologiesRequest.cs
@@ -1,15 +1,9 @@
-﻿using Quizz.Domain.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Quizz.Domain.Core.Dto
+﻿namespace Quizz.Domain.Core.Dto
 {
     public class TechnologiesRequest
     {
         public long? Id { get; set; }
         public string Name { get; set; }
+        public int AdminId { get; set; }
     }
 }
