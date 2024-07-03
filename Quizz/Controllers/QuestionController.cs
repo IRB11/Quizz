e030007b94c0be3c774464f244d1ee82146dfd8a
IB11 <ibrahimrabai11@gmail.Com>
2024-07-03 21:59:15 +0200

    Feat / Back : Question/Response Implement GetAllQuestion usecase in question controller

--- Quizz/Controllers/QuestionController.cs
+++ Quizz/Controllers/QuestionController.cs
@@ -11,17 +11,19 @@ namespace Quizz.Controllers
     {
         private readonly ICreateQuestion createQuestion;
         private readonly IGetQuestionById getQuestionById;
+        private readonly IGetAllQuestion getAllQuestion;
 
-        public QuestionController(ICreateQuestion createQuestion, IGetQuestionById getQuestionById)
+        public QuestionController(ICreateQuestion createQuestion, IGetQuestionById getQuestionById, IGetAllQuestion getAllQuestion)
         {
             this.createQuestion = createQuestion;
             this.getQuestionById = getQuestionById;
+            this.getAllQuestion = getAllQuestion;
         }
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            return Ok();
+            return Ok(await getAllQuestion.Handle());
         }
 
         [HttpGet("{id}")]
