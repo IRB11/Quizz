3adcb8c792d748a91e97fc16e04d14498a60853a
IB11 <ibrahimrabai11@gmail.Com>
2024-07-03 16:48:59 +0200

    Feat / Back : Question/Response add  question repository interface

---
+++ Quizz.Domain.Core/Interfaces/Questions/IQuestionRepository.cs
@@ -0,0 +1,14 @@
+﻿using Quizz.Domain.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.Interfaces.Questions
+{
+    public interface IQuestionRepository : ICRUDRepository<QuestionRequest, QuestionResponse>
+    {
+        Task<bool> QuestionExists(string content);
+    }
+}
