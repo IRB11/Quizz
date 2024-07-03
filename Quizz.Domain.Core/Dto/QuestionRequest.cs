363bca7e7e2c25213c9fd4ebe6b1c31ca008e07f
IB11 <ibrahimrabai11@gmail.Com>
2024-07-03 16:54:08 +0200

    Feat / Back : Question/Response adjust question request for mapping

--- Quizz.Domain.Core/Dto/QuestionRequest.cs
+++ Quizz.Domain.Core/Dto/QuestionRequest.cs
@@ -14,9 +14,10 @@ namespace Quizz.Domain.Core.Dto
         public string Type { get; set; }
         public bool IsValid { get; set; }
         public int Order {  get; set; }
-        public User Admin { get; set; }
-        public ICollection<Response>? Response { get; set; }
-        public Level Level { get; set; }
-        public Technology Technology { get; set;}
+
+        public int AdminId { get; set; }
+        public IList<Response_Request> Response { get; set; }
+        public int LevelId { get; set; }
+        public int TechnologyId { get; set;}
     }
 }
