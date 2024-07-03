ecf30b96b5f80d10a12627bcb11030430a5a9dfe
IB11 <ibrahimrabai11@gmail.Com>
2024-07-03 21:58:01 +0200

    Feat / Back : Question/Response Add interface and usecase GetAllQuestion

---
+++ Quizz.Domain.Core/UseCases/Question/GetAllQuestion.cs
@@ -0,0 +1,26 @@
+﻿using Quizz.Common.Interfaces;
+using Quizz.Domain.Core.Dto;
+using Quizz.Domain.Core.Interfaces.Questions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.UseCases.Question
+{
+    public class GetAllQuestion : IGetAllQuestion
+    {
+        IQuestionRepository _questionRepository;
+        IEnumerable<ICheckQuestionRule<QuestionRequest>> _rules;
+        public GetAllQuestion(IQuestionRepository questionRepository, IEnumerable<ICheckQuestionRule<QuestionRequest>>  rules)
+        {
+            _questionRepository = questionRepository;
+            _rules = rules;
+        }
+        public async Task<List<QuestionResponse>> Handle()
+        {
+           return await _questionRepository.getAll();
+        }
+    }
+}
