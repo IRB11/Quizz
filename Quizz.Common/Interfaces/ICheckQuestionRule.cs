1cc8268e50322d1393ccf50b22ce7b321988b967
IB11 <ibrahimrabai11@gmail.Com>
2024-07-03 16:50:57 +0200

    Feat / Back : Question/Response add  interface check question  rule

---
+++ Quizz.Common/Interfaces/ICheckQuestionRule.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Quizz.Common.Interfaces
+{
+    public interface ICheckQuestionRule<in TUseCaseRequest>
+    {
+        string GetErrorMessage();
+        Task<bool> CheckRule(TUseCaseRequest request);
+    }
+}
