546c99c00d8f4bc2e91ccee7d5b704cced6bf987
IB11 <ibrahimrabai11@gmail.Com>
2024-07-03 19:21:46 +0200

    Feat / Back : Question/Response Add interface and usecase GetQuestionById

---
+++ Quizz.Domain.Core/UseCases/Question/GetQuestionById.cs
@@ -0,0 +1,26 @@
+﻿using Quizz.Domain.Core.Dto;
+using Quizz.Domain.Core.Interfaces.Questions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.UseCases.Question
+{
+    public class GetQuestionById : IGetQuestionById
+    {
+        private readonly IQuestionRepository _questionRepository;
+        public GetQuestionById(IQuestionRepository questionRepository)
+        {
+            _questionRepository = questionRepository;
+        }
+
+
+
+        public async Task<QuestionResponse> Handle(int id)
+        {
+            return await _questionRepository.GetById(id);
+        }
+    }
+}
