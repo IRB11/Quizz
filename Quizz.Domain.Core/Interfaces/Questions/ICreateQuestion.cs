14321cfb8034c7c5b7e76551e87b34f7eb4f7b7c
IB11 <ibrahimrabai11@gmail.Com>
2024-07-03 16:49:20 +0200

    Feat / Back : Question/Response add create question interface

---
+++ Quizz.Domain.Core/Interfaces/Questions/ICreateQuestion.cs
@@ -0,0 +1,16 @@
+﻿using Quizz.Common.Decorator;
+using Quizz.Common.Interfaces;
+using Quizz.Domain.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.Interfaces.Questions
+{
+    public interface ICreateQuestion : IUseCaseRequestHandler<QuestionRequest, QuestionResponse>
+    {
+
+    }
+}
