2ce3b2dc04aa59b4d830a71375957cdfeaca8d67
Natalia <natalia@example.com7>
2024-07-08 14:40:11 +0200

    Modification Acceuil

--- Webapp/src/app/app.routes.ts
+++ Webapp/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: 'home' },
-    { path: 'home', loadComponent: () => import('../app/login-page/login-page.component').then(m => m.LoginPageComponent) },
+    { path: '', pathMatch: 'full', redirectTo: 'login' },
+    { path: 'login', loadComponent: () => import('../app/login-page/login-page.component').then(m => m.LoginPageComponent) },
+    { path: 'home', loadComponent : () => import('../app/home/home.component').then(m => m.HomeComponent) },
    // { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
     { path: '**', pathMatch: 'full', redirectTo: 'home' },   
 ]
--- Webapp/src/app/shortcuts/shortcuts.component.ts
+++ Webapp/src/app/shortcuts/shortcuts.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+interface Shortcut {
+  name: string;
+  link: string;
+  description?: string; // Description du lien
+  iconUrl: string; // URL de l'image de l'icône
+  genererquizz?: boolean;
+  agentmanager?: boolean;
+  planning?: boolean;
+  quizzbank?: boolean;
+  candidats?: boolean;
+  results?: boolean;
+  agentmanagement?: boolean;
+}
 
 @Component({
   selector: 'app-shortcuts',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './shortcuts.component.html',
-  styleUrl: './shortcuts.component.css'
+  styleUrls: ['./shortcuts.component.css']
 })
 export class ShortcutsComponent {
+  shortcuts: Shortcut[] = [
+    { name: 'Gestion Agents', link: '/Agent', iconUrl: '../../assets/img/iconagentmanager.png', description: 'Gérer les agents'},
+    { name: 'Gestion Candidats', link: '/Candidats', iconUrl: '../../assets/img/iconcandidats.png', description: 'Gérer les candidats'},
+    { name: 'Quizz Bank', link: '/quizz', iconUrl: '../../assets/img/iconquizzbank.png', description: 'Gérer question/réponses' },
+    { name: 'Gestion Quizz', link: '/quizz', iconUrl: '../../assets/img/icongenererquizz.png', description: 'Gérer quizz' },
+    { name: 'Resultats', link: '/results', iconUrl: '../../assets/img/iconresults.png', description: 'Générer résultats' },
+    { name: 'Planning', link: '/planning', iconUrl: '../../assets/img/iconplanning.png', description: 'Agenda' }
+  ];
 
 }
+
