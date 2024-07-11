d75c4d934c4cf2e576e6e12b5a135a59b651639b
IB11 <ibrahimrabai11@gmail.Com>
2024-07-11 10:29:58 +0200

    Feat/Quizz : Back add Interface IGetListQuestionByQuizzId and ISaveCandidateResponse

---
+++ Quizz.Domain.Core/Interfaces/Questions/IGetListQuestionByQuizzId.cs
@@ -0,0 +1,10 @@
+﻿using Quizz.Common.Interfaces;
+using Quizz.Domain.Core.Dto;
+
+namespace Quizz.Domain.Core.Interfaces.Questions
+{
+    public interface IGetListQuestionByQuizzId : IUseCaseRequestHandler<int, List<QuestionResponse>>
+    {
+
+    }
+}
---
+++ Quizz.Domain.Core/Interfaces/Questions/ISaveCandidateResponse.cs
@@ -0,0 +1,14 @@
+﻿using Quizz.Common.Interfaces;
+using Quizz.Domain.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.Interfaces.Questions
+{
+    public interface ISaveCandidateResponse : IUseCaseRequestHandler<CandidateResponse_Request, bool>
+    {
+    }
+}
