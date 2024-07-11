78d895b50c7906794f2e428b4cb053d7f4aa02c8
IB11 <ibrahimrabai11@gmail.Com>
2024-07-11 10:31:42 +0200

    Feat/Quizz : Back add interfaces IGenerateQuiz and IQuizzRepository

---
+++ Quizz.Domain.Core/Interfaces/Quizz/IGenerateQuiz.cs
@@ -0,0 +1,15 @@
+﻿using Quizz.Common.Interfaces;
+using Quizz.Domain.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.Interfaces.Quizz
+{
+    public interface IGenerateQuiz : IUseCaseRequestHandler<QuizRequest, QuizResponse>
+    {
+
+    }
+}
---
+++ Quizz.Domain.Core/Interfaces/Quizz/IQuizRepository.cs
@@ -0,0 +1,16 @@
+﻿using Quizz.Domain.Core.Dto;
+using Quizz.Domain.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.Interfaces.Quizz
+{
+    public interface IQuizzRepository : ICRUDRepository<QuizRequest, QuizResponse>
+    {
+        List<Quizz_QuestionResponse> GetQuestionsByQuizzId(int id);
+        Task<List<Quizz_QuestionResponse>> SaveQuestionToQuizz_Question(int id, List<Quizz_QuestionResponse> quiz_Questions);
+    }
+}
