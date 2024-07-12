be18a2db10a51ac1527c8fd525b492891ad77786
Natalia <natalia@example.com7>
2024-07-12 10:20:49 +0200

    Feat/results Creation Component PDF function

--- Webapp/src/app/results/results.component.spec.ts
+++ Webapp/src/app/results/results.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ResultsComponent } from './results.component';
+import ResultsComponent from './results.component';
 
 describe('ResultsComponent', () => {
   let component: ResultsComponent;
--- Webapp/src/app/results/results.component.ts
+++ Webapp/src/app/results/results.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-// import {jsPDF} from 'jspdf';
-import 'jspdf-autotable';
+import {jsPDF} from 'jspdf';
+import autoTable from 'jspdf-autotable'; // Added this line
 
 
 
@@ -13,14 +13,44 @@ import 'jspdf-autotable';
   templateUrl: './results.component.html',
   styleUrl: './results.component.css'
 })
-export default class ResultsComponent {
+export default class ResultsComponent implements OnInit {
   candidateNameOrUrl: string = '';
+  quizResults: any = null;
+  isLoading: boolean = true;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.http.get('http://localhost:5001/quizResults').subscribe({
+      next: (data: any) => {
+        this.quizResults = data;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Erreur lors de la récupération des résultats du quiz:', error);
+        this.isLoading = false;
+      }
+    });
+  }
 
   loadPDF(): void {
-    // Logique pour charger le PDF en fonction de candidateNameOrUrl
-    console.log('Charger le PDF pour:', this.candidateNameOrUrl);
+        if (this.quizResults != null) {
+          const doc = new jsPDF();
+          doc.text('Résultats du quiz', 50, 30);
+          const tableData = this.quizResults.map((result: any) => [result.candidateName, result.question, result.answer, result.correctAnswer, result.score, result.quizName]);
+          
+          autoTable(doc, { // Changed this line
+            head: [['Question', 'Réponse', 'Réponse Correcte']],
+            body: tableData
+          });
+
+          doc.save('results.pdf');
+          console.log('Charger le PDF pour:', this.candidateNameOrUrl);
+        }
+        else {
+          console.error('Aucun résultat du quiz trouvé');
+        }
     // Ajoutez ici la logique pour générer et charger le PDF en fonction de l'entrée de l'utilisateur
   }
 }
