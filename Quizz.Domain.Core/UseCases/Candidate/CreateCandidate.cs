95a0559c7128f93a0643c21b8fc7630ea4f74baa
yaxi57 <yaxi57@tutanota.com>
2024-07-12 09:47:02 +0200

    Feat/Create/Candidate

---
+++ Quizz.Domain.Core/UseCases/Candidate/CreateCandidate.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Quizz.Domain.Core.Dto;
+using Quizz.Domain.Core.Entities;
+using Quizz.Domain.Core.Interfaces;
+
+
+namespace Quizz.Domain.Core.UseCases.Candidate
+{
+    public class CreateCandidate : ICreateCandidate
+    {
+        private readonly ICandidateRepository candidateRepository;
+
+        public CreateCandidate(ICandidateRepository candidateRepository)
+        {
+            this.candidateRepository = candidateRepository;
+        }         
+
+        public async Task<CandidateResponse> Handle(CandidateRequest candidateRequest)
+        {
+            bool CandidateExist = await candidateRepository.CandidateAlreadyExist(candidateRequest);
+            if (!CandidateExist)
+            {
+                var response = await candidateRepository.Add(candidateRequest);
+
+                return response;
+            }
+            else return null;
+
+        }
+    
+    }
+
+
+    
+     
+    
+
+}
