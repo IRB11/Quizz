603c5259b2d0d1abd1709447fc1c8365044b2c3c
yaxi57 <yaxi57@tutanota.com>
2024-07-12 10:05:22 +0200

    Feat/Modifi/C_Reqe

--- Quizz.Domain.Core/Dto/CandidateRequest.cs
+++ Quizz.Domain.Core/Dto/CandidateRequest.cs
@@ -9,11 +9,11 @@ namespace Quizz.Domain.Core.Dto
 {
     public class CandidateRequest
     {
-        public long? Id { get; set; }
-        public string Name { get; set; }
+        public int  Id { get; set; }
+        public string FirstName { get; set; }
         public string Lastname { get; set; }
         public string PhoneNumber { get; set; }
-        public string EmailAdress { get; set; }
-        public User Agent { get; set; }
+        public string EmailAddress { get; set; }
+        public int AgentId { get; set; }
     }
 }
