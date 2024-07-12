d12f0631873f1fb0a82fa4043388f2b9cc5f33b3
yaxi57 <yaxi57@tutanota.com>
2024-07-12 09:49:55 +0200

    Feat/GetAll/Cantidate

---
+++ Quizz.Domain.Core/UseCases/Candidate/GetAllCandidate.cs
@@ -0,0 +1,29 @@
+﻿using Quizz.Domain.Core.Dto;
+using Quizz.Domain.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.UseCases.Candidate
+{
+    public class GetAllCandidate : IGetAllCandidates
+    {
+        private readonly ICandidateRepository _candidateRepository;
+
+        public GetAllCandidate()
+        {
+        }
+
+        public GetAllCandidate(ICandidateRepository candidateRepository)
+        {
+            _candidateRepository = candidateRepository;
+        }
+        public async Task<List<CandidateResponse>> Handle()
+        {
+            return await _candidateRepository.getAll();
+        }
+
+    }
+}
