d187f6ad0f7da96caff4c79656ccad9483937086
IB11 <ibrahimrabai11@gmail.Com>
2024-07-13 23:28:39 +0200

    Feat/Back : Quizz Add use case ans interface GetQuizzById

---
+++ Quizz.Domain.Core/UseCases/Quizz/GetQuizzById.cs
@@ -0,0 +1,26 @@
+﻿using Quizz.Common.Interfaces;
+using Quizz.Domain.Core.Dto;
+using Quizz.Domain.Core.Interfaces;
+using Quizz.Domain.Core.Interfaces.Quizz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz.Domain.Core.UseCases.Quizz
+{
+    public class GetQuizzById : IGetQuizzById
+    {
+       private readonly IQuizzRepository _quizzRepository;
+
+        public GetQuizzById(IQuizzRepository quizzRepository)
+        {
+            _quizzRepository = quizzRepository;
+        }
+        public async Task<QuizResponse> Handle(int id)
+        {
+           return await  _quizzRepository.GetById(id);
+        }
+    }
+}
