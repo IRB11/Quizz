2bc95edaf8f9aeb4516d4ed3fd21259fe223e9ec
IB11 <ibrahimrabai11@gmail.Com>
2024-07-14 00:20:30 +0200

    Feat/Back : Quizz Add Update use case and interface

---
+++ Quizz.Domain.Core/UseCases/Quizz/UpdateQuizz.cs
@@ -0,0 +1,18 @@
+﻿using Quizz.Domain.Core.Dto;
+using Quizz.Domain.Core.Interfaces.Quizz;
+
+namespace Quizz.Domain.Core.UseCases.Quizz
+{
+    public class UpdateQuizz : IUpdateQuizz
+    {
+        private readonly IQuizzRepository _quizzRepository;
+        public UpdateQuizz(IQuizzRepository quizzRepository)
+        {
+            _quizzRepository = quizzRepository;
+        }
+        public Task<QuizResponse> Handle(QuizRequest request)
+        {
+            return _quizzRepository.Update(request);
+        }
+    }
+}
