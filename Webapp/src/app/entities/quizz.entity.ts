d399743557f21dfc0235dfc5d6cbaf6562b3cc9d
IB11 <ibrahimrabai11@gmail.Com>
2024-07-14 19:38:34 +0200

    Feat/Back : Passage Quizz add Quizz entity

---
+++ Webapp/src/app/entities/quizz.entity.ts
@@ -0,0 +1,43 @@
+export interface Quizz {
+    candidateId: any;
+    agentId: any;
+    technologyId: any;
+    adminId: any;
+    id: number;
+    comment: string;
+    completionLevel: number;
+    completionTime: Date;
+    isValid: boolean;
+    numberOfQuestion: number;
+    quizzNumber: string;
+    result: number;
+    url: string;
+    
+    agent: any;
+    admin: any;
+    candidate: any;
+    statusId: number;
+    status: any;
+    level: any;
+    technologies: any;
+}
+
+export interface Responses {
+  // Define the properties of ResponseRequest here        public long? Id { get; set; }
+        id: number;
+        content: string;
+        explanation: string;
+        isCorrect: boolean;
+}
+export interface savedResponses {
+
+        id?: number;
+        content: string;
+        isSkipped: boolean;
+        openResponseText: string;
+        comment: string;
+        responseId: number;
+        quizId?: number;
+        questionId?: number;
+  }
+  
