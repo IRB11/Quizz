5ea7e942b7f262a76e87c8a0d7f86c7f13630660
IB11 <ibrahimrabai11@gmail.Com>
2024-07-14 19:32:01 +0200

    Feat/Passage Quizz : change candidate response to display in front

--- Quizz.Domain.Core/Dto/CandidateResponse.cs
+++ Quizz.Domain.Core/Dto/CandidateResponse.cs
@@ -14,7 +14,8 @@ namespace Quizz.Domain.Core.Dto
         public string Lastname { get; set; }
         public string PhoneNumber { get; set; }
         public string EmailAdress { get; set; }
-        public User Agent { get; set; }
-        public Quiz Quiz { get; set; }
+        public int AgentId { get; set; }
+        public UserResponse Agent { get; set; }
+        public QuizResponse Quiz { get; set; }
     }
 }
